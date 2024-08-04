b2cd7a05206f5e1f07b9da696984d56b1526d1b9
Adn110 <adacmilchtuete@gmail.com>
2024-08-04 22:26:21 +0200

    new input formats implemented

--- codes/main.py
+++ codes/main.py
@@ -1,13 +1,15 @@
 from VRP_double_depot import *
 import subprocess
 
-program = r'C:\Programmieren\AUFSTIEG\Tum-solver\uav_trajectory_planner_32d2238.exe'
+program = r'C:\Programmieren\AUFSTIEG\Tum-solver\uav_trajectory_planner_0d82111.exe'
 command = [program, 'plan', 'instance', r'C:\Programmieren\AUFSTIEG\codes\instance.json',
            r'C:\Programmieren\AUFSTIEG\codes\missions.json']
+commandtest = [program, 'plan', 'instance', r'C:\Programmieren\AUFSTIEG\Tum-solver\instance.json',
+           r'C:\Programmieren\AUFSTIEG\Tum-solver\missions.json']
 
 try:
     # Führe das Programm aus und fange die Ausgabe ein
-    result = subprocess.run(command, check=True, text=True, capture_output=True)
+    result = subprocess.run(commandtest, text=True, capture_output=True)
 
     # Gib die Ausgabe des Programms aus
     print("Program output:\n", result.stdout)
